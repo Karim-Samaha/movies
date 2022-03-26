dd6b810dfadcd79185c16ce6181798d2636ed85b
unknown <kareem.mahmoud1237@gmail.com>
2022-03-27 01:53:06 +0200

    update

---
+++ precache-manifest.36225ce80eb021a5bbe0e0de15d9a555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21e9e6cda4becefc70db37be5e80589",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "62d51b532f6a154ca8f8",
+    "url": "/movies/static/css/main.a962f211.chunk.css"
+  },
+  {
+    "revision": "c2761fa6a49949359558",
+    "url": "/movies/static/js/2.ae9edbd8.chunk.js"
+  },
+  {
+    "revision": "62d51b532f6a154ca8f8",
+    "url": "/movies/static/js/main.82917fb3.chunk.js"
+  },
+  {
+    "revision": "04129ba559ae92aa086e",
+    "url": "/movies/static/js/runtime~main.712e9881.js"
+  },
+  {
+    "revision": "5f9227f0bae5e9692120ed929d4384bb",
+    "url": "/movies/static/media/logo.5f9227f0.svg"
+  }
+]);
